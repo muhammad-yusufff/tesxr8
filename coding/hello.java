43c2b51e342100c5e8c2378345230aa8f16a8cd0
muhammad-yusufff <muhammad_mustofa_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:12:49 +0700

    Add files via upload

---
+++ coding/hello.java
@@ -0,0 +1,21 @@
+public class hello{
+    public static void main(String[] args) {
+
+        String nama="Yusuf";
+        String asalkota="Malang";
+        int usia=16;
+        double beratbadan=50.00;
+        float tinggibadan=163.0f;
+        char jeniskelamin='L';
+        boolean status= false;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Asal Kota: "+asalkota);
+        System.out.println("Usia: "+usia);
+        System.out.println("Berat Badan: "+beratbadan);
+        System.out.println("Tinggi Badan: "+tinggibadan);
+        System.out.println("Jenis Kelamin: "+jeniskelamin);
+        System.out.println("Status: "+status);
+
+    }
+}   
